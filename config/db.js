41d98b28a856060e805909f5422d364b8f7adef5
vedant-vijay <vedantrokade159@gmail.com>
2025-12-30 13:07:00 +0530

    Remove MongoDB connection console logs
    Remove console logs for MongoDB connection status.

--- config/db.js
+++ config/db.js
@@ -3,9 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log('MongoDB connected');
   } catch (error) {
-    console.error('MongoDB connection failed');
     throw new Error('MongoDB connection failed');
   }
 };
